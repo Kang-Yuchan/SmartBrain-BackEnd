6847d52dfbf485ea6e071da1d15b1234c56141fd
Kang.Yuchan <ikimaku0517@gmail.com>
2019-01-22 05:08:59 +0900

    conec db

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-horizontal-25402',
-    user : 'kang-yuchan',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
