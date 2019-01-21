aceab7b65d5e4f154c304dde08ec8e59acfe25f1
Kang.Yuchan <ikimaku0517@gmail.com>
2019-01-22 05:45:27 +0900

    up to github

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-   connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'kang-yuchan',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
 
 const app = express();
 
@@ -29,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, ()=> {
+  console.log(`app is running on port 3000`);
 })
